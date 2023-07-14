739655fe4f634f58c0bb8cdcc0bcbf3334553e40
JMaddison-Tympa <107916747+JMaddison-Tympa@users.noreply.github.com>
2023-07-14 17:31:39 +0100

    Update index.js
    fix

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const sineWaveArray = new Float32Array(sampleRate);
 const hz = 500;
 
 // fill all 44100 elements of array with Math.sin() values
-for (i = 0; i < sineWaveArray.length; i+=) {
+for (i = 0; i < sineWaveArray.length; i+=1) {
   //sineWaveArray[i] = Math.sin(i * Math.PI * 8 / hz);
   sineWaveArray[i] = Math.sin(i * 2*Math.PI * hz / sampleRate);
 }
